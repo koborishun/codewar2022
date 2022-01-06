495e8a65d59dad1d8afe376d63ee425efdd0799e
koborishun <koboshun0723@icloud.com>
2022-01-06 21:40:14 +0900

    戻り値を必ずマイナスにする

---
+++ src/eight/ReturnNegative.java
@@ -0,0 +1,12 @@
+package eight;
+
+//In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+public class ReturnNegative {
+	public static int makeNegative(final int x) {
+		return x >= 0 ? -x : x;
+	}
+	//別解
+	//	public static int makeNegative(final int x) {
+	//		return -Math.abs(x);
+	//	}
+}
