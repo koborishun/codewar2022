46474cc211ff9b38849058667608c68d962a410d
koborishun <koboshun0723@icloud.com>
2022-01-17 22:53:39 +0900

    Revert "引数文字列の先頭と最後の文字を削除して改めて戻す"
    This reverts commit 8114fe707a9698a6fb930cf51f0262504784e731.

--- src/eight/RemoveFirstAndLastCharacter.java
+++
@@ -1,8 +0,0 @@
-package eight;
-
-//It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
-public class RemoveFirstAndLastCharacter {
-	public static String remove(String str) {
-		return str.substring(1, str.length() - 1);
-	}
-}
